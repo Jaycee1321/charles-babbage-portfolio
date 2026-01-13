24405ce5689e53ab1d140cd16a467978b594caf0
jayceel1321 <jayceel1321@gmail.com>
2026-01-13 19:29:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <NavBar/>
+
+  <div class="container-fluid">
+    <About/>
+    <Projects/>
+    <Tools/>
+    <Contacts/>
+  </div>
+
+  <FooterComponent/>
+
+</template>
+
+<script setup>
+  import NavBar from './components/NavBar.vue'
+  import About from './components/About.vue'
+  import Projects from './components/Projects.vue'
+  import Tools from './components/Tools.vue'
+  import Contacts from './components/Contacts.vue'
+  import FooterComponent from './components/FooterComponent.vue'
+</script>
